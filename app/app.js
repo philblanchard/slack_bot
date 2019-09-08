4e7c46abc26801de4161b33dfbe3f08435d3083f
philblanchard <phil@philblanchard.com>
2019-09-08 13:40:38 -0400

    added configs for channel topic changes

--- app/app.js
+++ app/app.js
@@ -1,21 +1,24 @@
 console.log(require('dotenv').config({debug: true}));
 const token = process.env.SLACK_BOT_TOKEN
+const usertoken = process.env.SLACK_USER_TOKEN
 
 const slack = require('slack')
 const bot = new slack({token})
+const user = new slack){{usertoken}}
+
 const regex = /<@\w+>/
 var app = {
     message: function(channel, text){
         console.log(`the channel is ${channel} \n the message is ${text}`)
         match = text.match(regex)
         console.log(`the match is ${match}`)
-        bot.chat.postMessage({channel: `${channel}`, text:`${match}`}).catch(function(err){
+        bot.chat.postMessage({channel: `${channel}`, text:`The concierge is: ${match}`}).catch(function(err){
             console.log(err)
         })
     },
 
     topic: function(channel, topic){
-        bot.channels.setTopic({channel: `${channel}`, topic: `${topic}`}).catch(function(err){
+        user.channels.setTopic({channel: `${channel}`, topic: `${topic}`}).catch(function(err){
             console.log(err)
         })
     }
